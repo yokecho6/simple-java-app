74fc28d0bad8455ea2dde489508fb00c7bebe5a1
yokecho6 <yokesiencho@gmail.com>
2026-04-06 16:43:25 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("random change to force a build on Jenkins");
+        System.out.println("new change to demo Webhook on jenkins");
     }
 
     public String getMessage() {
