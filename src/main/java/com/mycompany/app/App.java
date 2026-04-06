5cca74f2571851f426e367df102177436cb00e3f
yokecho6 <yokesiencho@gmail.com>
2026-04-06 15:16:30 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is boring!!!");
+        System.out.println("modify from local project");
     }
 
     public String getMessage() {
