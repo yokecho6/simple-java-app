0c51d629082ba44c0a70f5d5092f408b9fddf30c
yokecho6 <yokesiencho@gmail.com>
2026-04-07 11:10:26 +0800

    check out email-corrected

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,12 +5,12 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I hate Jenkins";
+    private static final String MESSAGE = "I love Jenkins";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("new change to demo email on jenkins");
+        System.out.println("new change to demo webhooks on jenkins");
     }
 
     public String getMessage() {
