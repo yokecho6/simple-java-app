73f7c644d812048f34e7593498da5c98d0464bfa
yokecho6 <yokesiencho@gmail.com>
2026-04-07 11:05:15 +0800

    check out email

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("new change to demo Webhook on jenkins");
+        System.out.println("new change to demo email on jenkins");
     }
 
     public String getMessage() {
